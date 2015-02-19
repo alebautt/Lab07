ac6eb6bff2c364798a7a3c4c5ba16445269323b8
alebautt <id_bautista@hotmail.com>
2015-02-19 01:10:15 -0600

    Initial Commit

---
+++ Lugares/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Lugares
+//
+//  Created by Alejandra B on 19/02/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Lugares/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lugares
+//
+//  Created by Alejandra B on 19/02/15.
+//  Copyright (c) 2015 alebautista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
